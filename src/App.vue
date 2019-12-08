6a8719169107a95e7be6fb2dabd29104857b4cc6
Administrator <zykzml7788@sina.com>
2019-12-09 00:48:26 +0800

    dev:侧栏样式修改

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
-  <div style="">
+
     <div id="app" v-if="this.$route.path==='/login'" style="height: 2000px;margin:0;background-image: url('../static/img/loginbgp.jpeg'); background-repeat: repeat;">
       <router-view/>
     </div>
     <el-container v-else>
-      <el-aside width="200px">
+      <el-aside width="200px" class="el-aside">
         <headers></headers>
       </el-aside>
       <el-main style="height: 90%">
-        <el-header id="el-header" height="10%"></el-header>
+        <el-header id="el-header" height="10%">
+
+        </el-header>
         <div id="app">
           <router-view/>
         </div>
       </el-main>
     </el-container>
-  </div>
 </template>
 
 <script>
@@ -37,6 +38,8 @@
 }
 
   #el-header {
-    background-image: url("../static/img/header.jpg");
+    background-color: dodgerblue;
+    height: 300px;
   }
+
 </style>
--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,41 +1,69 @@
 <template>
 
-  <div id="header">
-    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
-      <el-radio-button :label="false">展开</el-radio-button>
-      <el-radio-button :label="true">收起</el-radio-button>
-    </el-radio-group>
-    <el-menu default-active="1" class="el-menu-vertical-demo" @select="handleSelect" :collapse="isCollapse" id="cehuaMenu" width="20%">
+  <div id="header" style="background-color: dodgerblue;margin-top: 0">
+<!--    <div style="background-color: coral">-->
+<!--      <img src="../../static/img/mogu.jpg" alt="" style="height: 20%;width: 20%">Teemo-->
+<!--    </div>-->
+    <div style="height: 80px; margin: 20px" >
+      <h2 style="color: white">Teemo</h2>
+      <p style="font-weight: normal;font-size: small;color: #eaeaea">AutoTest For Api</p>
+    </div>
+    <div id="user" style="margin: 20px">
+      <div style="display: inline-block;width: 20%;height: 100%"><img src="../../static/img/user.jpg" style="width:  100%;height: 100%" alt=""></div>
+      <div style="display: inline-block;">
+        <div><strong class="el-icon-user-solid" style="color: white">test</strong></div>
+        <i class="el-icon-switch-button" style="color:red;"><strong style="font-size: small">注销</strong></i>
+      </div>
+    </div>
+
+    <el-menu default-active="1" class="el-menu-vertical-demo" @select="handleSelect" :collapse="isCollapse" id="cehuaMenu" width="20%" background-color="dodgerblue"
+             text-color="white" active-text-color="black">
       <el-menu-item index="1">
-        <i class="el-icon-menu"></i>
+        <i class="el-icon-menu" style="color:white"></i>
         <span slot="title">主页</span>
       </el-menu-item>
       <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
+        <i class="el-icon-menu" style="color:white"></i>
         <span slot="title">项目管理</span>
       </el-menu-item>
       <el-submenu index="3">
         <template slot="title">
-          <i class="el-icon-location"></i>
+          <i class="el-icon-location" style="color:white"></i>
           <span slot="title">用例管理</span>
         </template>
         <el-menu-item index="3-1">用例集管理</el-menu-item>
       </el-submenu>
       <el-menu-item index="4">
-        <i class="el-icon-document"></i>
+        <i class="el-icon-document" style="color:white"></i>
         <span slot="title">任务管理</span>
       </el-menu-item>
       <el-submenu index="5">
         <template slot="title">
-          <i class="el-icon-location"></i>
+          <i class="el-icon-location" style="color:white"></i>
           <span slot="title">数据库管理</span>
         </template>
         <el-menu-item index="5-1">数据源管理</el-menu-item>
         <el-menu-item index="5-2">脚本管理</el-menu-item>
       </el-submenu>
+      <el-submenu index="6">
+        <template slot="title">
+          <i class="el-icon-location" style="color:white"></i>
+          <span slot="title">用户管理</span>
+        </template>
+        <el-menu-item index="6-1">用户管理</el-menu-item>
+        <el-menu-item index="6-2">角色管理</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="7">
+        <i class="el-icon-document" style="color:white"></i>
+        <span slot="title">调试工具</span>
+      </el-menu-item>
       <el-menu-item index="6">
-        <i class="el-icon-document"></i>
-        <span slot="title">用户管理</span>
+        <i class="el-icon-document" style="color:white"></i>
+        <span slot="title">函数助手</span>
+      </el-menu-item>
+      <el-menu-item index="6">
+        <i class="el-icon-document" style="color:white"></i>
+        <span slot="title">个人中心</span>
       </el-menu-item>
     </el-menu>
 
@@ -95,7 +123,6 @@
 <style scoped>
   #header {
     font-weight: bold;
-
   }
   #cehuaMenu {
   }
