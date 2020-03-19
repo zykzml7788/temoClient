c74e758a9aafa438e80d3ce140264b32a151b094
Administrator <zykzml7788@sina.com>
2020-03-19 22:38:51 +0800

    dev:主页开发

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,11 +1,44 @@
 <template>
-  <div>
+  <div id="main">
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>/</el-breadcrumb-item>
     </el-breadcrumb>
-    <ve-line :data="chartData" :settings="time"></ve-line>
-    <ve-histogram :data="chartData2"></ve-histogram>
+    <div style="margin-top: 50px">
+      <el-card shadow="hover" class="card">
+        <div slot="header" class="chead">
+          <span>自动化已运行</span>
+          <el-tag effect="dark" class="tag" size="mini">持续</el-tag>
+        </div>
+        <span class="content" style="color: deepskyblue"> 0 天 0 小时 0 分钟</span>
+        <div class="bottom"><span>正在执行任务 10 个</span></div>
+      </el-card>
+      <el-card shadow="hover" class="card">
+        <div slot="header" class="chead">
+          <span>今日执行情况</span>
+          <el-tag effect="dark" class="tag" size="mini" type="warning">运行</el-tag>
+        </div>
+        <span  class="content" style="color: #ffcd32">已执行用例 300 条</span>
+        <div class="bottom"><span>成功率 90% </span></div>
+      </el-card>
+      <el-card shadow="hover" class="card">
+        <div slot="header" class="chead">
+          <span>用例库</span>
+          <el-tag effect="dark" class="tag" size="mini" type="danger">库存</el-tag>
+        </div>
+        <span  class="content" style="color: #ff4d51;">用例集库存 100 条</span>
+        <div class="bottom"><span>包含用例 1000 条</span></div>
+      </el-card>
+      <el-card shadow="hover" class="card">
+        <div slot="header" class="chead">
+          <span>任务库</span>
+          <el-tag effect="dark" class="tag" size="mini" type="success">库存</el-tag>
+        </div>
+        <span  class="content" style="color: #13ce66">普通任务 20 个</span>
+        <div class="bottom"><span>已开启定时任务  10 个</span></div>
+      </el-card>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -13,29 +46,45 @@
     export default {
         data() {
             return {
-                chartData: {
-                    columns: ['日期', '用例执行数', '成功数', '失败数'],
-                    rows: [
-                        {'日期': '2018-01-01', '用例执行数': 1393, '成功数': 1093, '失败数': 300},
-                        {'日期': '2018-01-02', '用例执行数': 3530, '成功数': 3230, '失败数': 300},
-                        {'日期': '2018-01-03', '用例执行数': 2923, '成功数': 2623, '失败数': 300},
-                        {'日期': '2018-01-05', '用例执行数': 1723, '成功数': 1423, '失败数': 300},
-                        {'日期': '2018-01-10', '用例执行数': 3792, '成功数': 3492, '失败数': 300},
-                        {'日期': '2018-01-20', '用例执行数': 4593, '成功数': 4293, '失败数': 300}
-                    ]
-                },
-                chartData2: {
-                    columns: ['日期', '用例执行数', '失败数'],
-                    rows: [
-                        { '日期': '2018-01-01', '用例执行数': 1393, '失败数': 1093},
-                        { '日期': '2018-01-01', '用例执行数': 3530, '失败数': 3230},
-                        { '日期': '2018-01-01', '用例执行数': 2923, '失败数': 2623},
-                        { '日期': '2018-01-01', '用例执行数': 1723, '失败数': 1423},
-                        { '日期': '2018-01-01', '用例执行数': 3792, '失败数': 3492},
-                        { '日期': '2018-01-01', '用例执行数': 4593, '失败数': 4293}
-                    ]
-                }
+
             }
         }
     }
 </script>
+
+<style scoped>
+
+  #main{
+    font-family: "Microsoft YaHei UI",serif;
+  }
+
+  .card {
+    float: left;
+    margin: 2%;
+    width:  20%;
+    height: 200px;
+    border-radius:10px;
+    text-align: left;
+
+  }
+
+  .chead {
+    text-align: left;
+    font-size: x-small;
+    font-weight: bold;
+  }
+
+  .content {
+    font-size: large;
+  }
+
+  .bottom {
+    margin-top: 50PX;
+    font-size: x-small;
+    font-weight: bold;
+  }
+
+  .tag {
+    float: right;
+  }
+</style>
