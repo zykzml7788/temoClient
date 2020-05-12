545f66712a674cc8f0a5896bc5e6bb825a4f1193
zykzml7788 <ykzhang@creams.io>
2020-05-12 11:53:08 +0800

    dev：钉钉页面开发

--- src/views/config/DingTalk.vue
+++ src/views/config/DingTalk.vue
@@ -46,7 +46,7 @@
         <template slot-scope="scope">
           <div style="margin-bottom: 10px;">
             <el-button
-              @click.native.prevent="lookDetail(scope.row.descId)"
+              @click.native.prevent="openTest(scope.row.descId)"
               size="mini"
               type="success">
               调试
@@ -86,6 +86,30 @@
         <el-button @click="closeDing">取 消</el-button>
       </div>
     </el-dialog>
+
+    <el-dialog title="机器人调试" :visible.sync="testShow" @close="closeTestDing">
+      <el-form :model="form2" :rules="rules2" ref="form2">
+        <el-form-item label="消息内容" :label-width="formLabelWidth" prop="content">
+          <el-input placeholder="请输入消息内容" v-model="form2.content" autocomplete="off" type="textarea"
+                    :autosize="{ minRows: 3, maxRows: 5}"></el-input>
+        </el-form-item>
+        <el-form-item label="需要@的手机号" :label-width="formLabelWidth" prop="atMobiles">
+          <el-input placeholder="请输入需要@的手机号(多个手机号用英文分号隔开)" v-model="form2.atMobiles" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="是否@全部" :label-width="formLabelWidth" prop="isAtAll" style="text-align: left">
+          <el-switch
+            v-model="form2.isAtAll"
+            active-color="#13ce66"
+            inactive-color="#ff4949">
+          </el-switch>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="success" @click="testDing">发 送</el-button>
+        <el-button @click="closeTestDing">取 消</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 
 </template>
@@ -124,9 +148,47 @@
         }
         this.dingShow = true;
       },
+      openTest(id){
+        this.testShow = true;
+        this.$axios.get('/apis/Dingding/'+id).then(res=>{
+            if (res.data.code === 200){
+              this.dingId = res.data.data.descId;
+            } else {
+              this.$notify({title:'操作失败',type:'warning',message:res.data.msg});
+            }
+            this.loading = false;
+          }
+        ).catch(err=>{
+          this.$notify({title:'操作失败',type:'error',message:err});
+        });
+      },
+      testDing(){
+        let body = {
+          isAtAll: this.form2.isAtAll,
+          content: this.form2.content,
+          atMobiles: this.form2.atMobiles.split(";")
+        };
+        this.$refs['form2'].validate(bol=>{
+            if (bol){
+              this.$axios.post('/apis/Dingding/sendText/'+this.dingId,body).then(res=>{
+                if (res.data.code === 200){
+                  this.$notify({title:'操作成功',type:'success',message:res.data.msg});
+                } else {
+                  this.$notify({title:'操作失败',type:'warning',message:res.data.msg});
+                }
+                this.loading = false;
+              });
+            }
+        });
+
+      },
       closeDing(){
         this.$refs['form'].resetFields();
         this.dingShow = false;
+      },
+      closeTestDing(){
+        this.$refs['form2'].resetFields();
+        this.testShow = false;
       },
       getDings(){
         this.loading = true;
@@ -145,35 +207,45 @@
       },
       saveDing(type){
         if (type==='add'){
-          this.$axios.post('/apis/Dingding/',this.form).then(res=>{
-              if (res.data.code === 200){
-                this.$notify({title:'操作成功',type:'success',message:res.data.msg});
-                this.closeDing();
-                this.getDings();
-              } else {
-                this.$notify({title:'操作失败',type:'warning',message:res.data.msg});
-              }
-              this.loading = false;
-            }
+          this.$refs['form'].validate(bol=>{
+            if (bol){
+              this.$axios.post('/apis/Dingding/',this.form).then(res=>{
+                  if (res.data.code === 200){
+                    this.$notify({title:'操作成功',type:'success',message:res.data.msg});
+                    this.closeDing();
+                    this.getDings();
+                  } else {
+                    this.$notify({title:'操作失败',type:'warning',message:res.data.msg});
+                  }
+                  this.loading = false;
+                }
 
-          ).catch(err=>{
-            this.$notify({title:'操作失败',type:'error',message:err});
+              ).catch(err=>{
+                this.$notify({title:'操作失败',type:'error',message:err});
+              });
+            }
           });
+
         } else {
-          this.$axios.put('/apis/Dingding/update/',this.form).then(res=>{
-              if (res.data.code === 200){
-                this.$notify({title:'操作成功',type:'success',message:res.data.msg});
-                this.closeDing();
-                this.getDings();
-              } else {
-                this.$notify({title:'操作失败',type:'warning',message:res.data.msg});
-              }
-              this.loading = false;
-            }
+          this.$refs['form'].validate(bol=>{
+            if (bol){
+              this.$axios.put('/apis/Dingding/update/',this.form).then(res=>{
+                  if (res.data.code === 200){
+                    this.$notify({title:'操作成功',type:'success',message:res.data.msg});
+                    this.closeDing();
+                    this.getDings();
+                  } else {
+                    this.$notify({title:'操作失败',type:'warning',message:res.data.msg});
+                  }
+                  this.loading = false;
+                }
 
-          ).catch(err=>{
-            this.$notify({title:'操作失败',type:'error',message:err});
+              ).catch(err=>{
+                this.$notify({title:'操作失败',type:'error',message:err});
+              });
+            }
           });
+
         }
 
       },
@@ -195,8 +267,10 @@
     },
     data() {
       return {
+        dingId:'',
         dialogTitle: '',
         button: '',
+        testShow:false,
         rules:{
           webhook:[
             {required:true,message:'请输入webhook',trigger:'change'},
@@ -204,7 +278,11 @@
           keysWord:[
             {required:true,message:'请输入keyWord',trigger:'change'},
           ]
-
+        },
+        rules2:{
+          content:[
+            {required:true,message:'请输入消息内容',trigger:'change'},
+          ]
         },
         page:1,
         total: 0,
@@ -223,7 +301,12 @@
           webhook:'',
           keysWord:''
         },
-        formLabelWidth:'100px'
+        form2: {
+          isAtAll: false,
+          content: '',
+          atMobiles: ''
+        },
+        formLabelWidth:'130px'
       }
     },
     components:{
